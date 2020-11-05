7053a01841af1474793ed8706623e6d5ae903f6b
Felix Haller <haller@ubique.ch>
2020-11-05 10:15:27 +0100

    Return traceKeys as protobuf

--- n2step-sdk-backend/n2step-sdk-backend-ws/src/main/java/ch/ubique/n2step/sdk/backend/ws/controller/N2StepController.java
+++ n2step-sdk-backend/n2step-sdk-backend-ws/src/main/java/ch/ubique/n2step/sdk/backend/ws/controller/N2StepController.java
@@ -1,10 +1,14 @@
 package ch.ubique.n2step.sdk.backend.ws.controller;
 
 import ch.ubique.n2step.sdk.backend.data.N2StepDataService;
+import ch.ubique.n2step.sdk.backend.model.ProblematicEventWrapperOuterClass.ProblematicEvent;
+import ch.ubique.n2step.sdk.backend.model.ProblematicEventWrapperOuterClass.ProblematicEventWrapper;
 import ch.ubique.n2step.sdk.backend.model.TraceKey;
 import ch.ubique.n2step.sdk.backend.model.util.DateUtil;
 import ch.ubique.openapi.docannotations.Documentation;
+import com.google.protobuf.ByteString;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,10 +33,38 @@ public class N2StepController {
         return ResponseEntity.ok().header("X-HELLO", "n2step").body("Hello from N2STEP WS v1");
     }
 
-    @GetMapping(value = "/traceKeys")
-    public @ResponseBody ResponseEntity<List<TraceKey>> getTraceKeys(
+    @GetMapping(
+            value = "/traceKeys",
+            produces = {"application/json"})
+    public @ResponseBody ResponseEntity<List<TraceKey>> getTraceKeysJson(
             @RequestParam(required = false) Long lastSync) {
         return ResponseEntity.ok()
                 .body(dataService.findTraceKeys(DateUtil.toLocalDateTime(lastSync)));
+    }
+
+    @GetMapping(
+            value = "/traceKeys",
+            produces = {"application/protobuf"})
+    public @ResponseBody ResponseEntity<byte[]> getTraceKeys(
+            @RequestParam(required = false) Long lastSync) {
+        List<TraceKey> traceKeys = dataService.findTraceKeys(DateUtil.toLocalDateTime(lastSync));
+        ProblematicEventWrapper pew =
+                ProblematicEventWrapper.newBuilder()
+                        .setVersion(1)
+                        .addAllEvents(mapToProblematicEvents(traceKeys))
+                        .build();
+        return ResponseEntity.ok().body(pew.toByteArray());
+    }
+
+    private List<ProblematicEvent> mapToProblematicEvents(List<TraceKey> traceKeys) {
+        return traceKeys.stream()
+                .map(
+                        t ->
+                                ProblematicEvent.newBuilder()
+                                        .setSecretKey(ByteString.copyFrom(t.getSecretKey()))
+                                        .setStartTime(DateUtil.toEpochMilli(t.getStartTime()))
+                                        .setEndTime(DateUtil.toEpochMilli(t.getEndTime()))
+                                        .build())
+                .collect(Collectors.toList());
     }
 }
