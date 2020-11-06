befe7a5ba6bb03683acbffdb70839fbf8adc9569
alig <alig@ubique.ch>
2020-11-06 14:37:39 +0100

    Make lazysodium work in SpringBoot Jar.

--- n2step-sdk-backend/n2step-sdk-backend-ws/src/main/java/ch/ubique/n2step/sdk/backend/ws/SodiumWrapper.java
+++ n2step-sdk-backend/n2step-sdk-backend-ws/src/main/java/ch/ubique/n2step/sdk/backend/ws/SodiumWrapper.java
@@ -1,5 +1,11 @@
 package ch.ubique.n2step.sdk.backend.ws;
 
+import java.io.File;
+import java.io.InputStream;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 import org.slf4j.Logger;
@@ -8,7 +14,9 @@ import org.slf4j.LoggerFactory;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.goterl.lazycode.lazysodium.SodiumJava;
 import com.goterl.lazycode.lazysodium.interfaces.Box;
-import com.goterl.lazycode.lazysodium.utils.LibraryLoader.Mode;
+import com.goterl.lazycode.lazysodium.utils.LibraryLoadingException;
+import com.sun.jna.Native;
+import com.sun.jna.Platform;
 
 import ch.ubique.n2step.sdk.backend.model.SeedMessageOuterClass;
 import ch.ubique.n2step.sdk.backend.model.SeedMessageOuterClass.SeedMessage;
@@ -34,7 +42,19 @@ public class SodiumWrapper {
 			logger.error("unable to parse pk hexstring", e);
 			throw new RuntimeException(e);
 		}
-		this.sodium = new SodiumJava(Mode.PREFER_BUNDLED);
+		// Do custom loading for the libsodium lib, as it does not work out of the box
+		// with spring boot bundled jars. To get a path to the full file, we copy
+		// libsodium to a tmpfile and give that absolute path to lazysodium
+		try {
+			InputStream in = getClass().getClassLoader().getResourceAsStream("libsodium/" + getSodiumPathInResources());
+			File libTmpFile = File.createTempFile("libsodium", null);
+			Files.copy(in, libTmpFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+			in.close();
+			this.sodium = new SodiumJava(libTmpFile.getAbsolutePath());
+		} catch (Exception e) {
+			logger.error("unable to load libsodium", e);
+			throw new RuntimeException(e);
+		}
 	}
 
 	public byte[] decryptQrTrace(byte[] ctx) throws InvalidProtocolBufferException {
@@ -47,5 +67,42 @@ public class SodiumWrapper {
 		sodium.crypto_sign_seed_keypair(newPk, newSk, msg);
 		logger.debug(newSk.toString());
 		return newSk;
+	}
+
+	/**
+	 * Returns the absolute path to sodium library inside JAR (beginning with '/'),
+	 * e.g. /linux/libsodium.so.
+	 */
+	private static String getSodiumPathInResources() {
+		boolean is64Bit = Native.POINTER_SIZE == 8;
+		if (Platform.isWindows()) {
+			if (is64Bit) {
+				return getPath("windows64", "libsodium.dll");
+			} else {
+				return getPath("windows", "libsodium.dll");
+			}
+		}
+		if (Platform.isARM()) {
+			return getPath("armv6", "libsodium.so");
+		}
+		if (Platform.isLinux()) {
+			if (is64Bit) {
+				return getPath("linux64", "libsodium.so");
+			} else {
+				return getPath("linux", "libsodium.so");
+			}
+		}
+		if (Platform.isMac()) {
+			return getPath("mac", "libsodium.dylib");
+		}
+
+		String message = String.format("Unsupported platform: %s/%s", System.getProperty("os.name"),
+				System.getProperty("os.arch"));
+		throw new LibraryLoadingException(message);
+	}
+
+	private static String getPath(String folder, String name) {
+		String separator = "/";
+		return folder + separator + name;
 	}
 }
