b2a8042f59e9e74b2726bb16d3101d47a8cddb0d
Severin Kistler <kistler@ubique.ch>
2021-02-08 17:02:53 +0100

    Remove unused argument

--- notifyme-sdk-backend/notifyme-sdk-backend-ws/src/main/java/ch/ubique/notifyme/sdk/backend/ws/controller/web/WebController.java
+++ notifyme-sdk-backend/notifyme-sdk-backend-ws/src/main/java/ch/ubique/notifyme/sdk/backend/ws/controller/web/WebController.java
@@ -1,7 +1,6 @@
 package ch.ubique.notifyme.sdk.backend.ws.controller.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WebController {
 
     @GetMapping("/")
-    public String web(final Model model) {
+    public String web() {
         return "home";
     }
 }
