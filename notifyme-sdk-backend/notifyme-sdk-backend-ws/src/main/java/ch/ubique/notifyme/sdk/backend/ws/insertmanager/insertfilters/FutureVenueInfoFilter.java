f2c7f5d7d0485263e0a8cb4f20a33d30f36edf4f
lmeinen <lmeinen@student.ethz.ch>
2021-05-25 15:35:33 +0200

    Resolved: Should ensure no future VenueInfo objects are uploaded

---
+++ notifyme-sdk-backend/notifyme-sdk-backend-ws/src/main/java/ch/ubique/notifyme/sdk/backend/ws/insertmanager/insertfilters/FutureVenueInfoFilter.java
@@ -0,0 +1,25 @@
+package ch.ubique.notifyme.sdk.backend.ws.insertmanager.insertfilters;
+
+import ch.ubique.notifyme.sdk.backend.model.UserUploadPayloadOuterClass.UploadVenueInfo;
+import ch.ubique.notifyme.sdk.backend.ws.insertmanager.InsertException;
+import ch.ubique.notifyme.sdk.backend.ws.insertmanager.OSType;
+import ch.ubique.notifyme.sdk.backend.ws.semver.Version;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FutureVenueInfoFilter implements
+    UploadInsertionFilter {
+
+  @Override
+  public List<UploadVenueInfo> filter(LocalDateTime now, List<UploadVenueInfo> uploadVenueInfoList,
+      OSType osType, Version osVersion, Version appVersion, Object principal)
+      throws InsertException {
+    return uploadVenueInfoList.stream().filter(uploadVenueInfo -> {
+      final var intervalEnd = LocalDateTime.ofInstant(Instant.ofEpochMilli(uploadVenueInfo.getIntervalEndMs()), ZoneOffset.UTC);
+      return intervalEnd.isBefore(now);
+    }).collect(Collectors.toList());
+  }
+}
