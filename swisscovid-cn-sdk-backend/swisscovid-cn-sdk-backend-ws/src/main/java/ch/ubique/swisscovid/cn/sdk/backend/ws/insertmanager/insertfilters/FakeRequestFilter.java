adf30de86c1d2a29a7616f4cde7af7321e5414a2
Martin Alig <alig@ubique.ch>
2021-06-21 10:02:04 +0200

    Throw exception if fake field is not valid.

--- swisscovid-cn-sdk-backend/swisscovid-cn-sdk-backend-ws/src/main/java/ch/ubique/swisscovid/cn/sdk/backend/ws/insertmanager/insertfilters/FakeRequestFilter.java
+++ swisscovid-cn-sdk-backend/swisscovid-cn-sdk-backend-ws/src/main/java/ch/ubique/swisscovid/cn/sdk/backend/ws/insertmanager/insertfilters/FakeRequestFilter.java
@@ -4,6 +4,7 @@ import ch.ubique.swisscovid.cn.sdk.backend.model.UserUploadPayloadOuterClass;
 import ch.ubique.swisscovid.cn.sdk.backend.model.UserUploadPayloadOuterClass.UploadVenueInfo;
 import ch.ubique.swisscovid.cn.sdk.backend.ws.insertmanager.InsertException;
 
+import ch.ubique.swisscovid.cn.sdk.backend.ws.insertmanager.InvalidFormatException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +20,11 @@ public class FakeRequestFilter implements UploadInsertionFilter {
       List<UploadVenueInfo> uploadVenueInfoList,
       Object principal)
       throws InsertException {
+    for (UploadVenueInfo uploadVenueInfo : uploadVenueInfoList) {
+      if (uploadVenueInfo.getFake().toByteArray().length != 1) {
+        throw new InvalidFormatException();
+      }
+    }
     return uploadVenueInfoList.stream()
         .filter(uploadVenueInfo -> (uploadVenueInfo.getFake().toByteArray()[0] == 0)) // only collect real venue infos.
         .collect(Collectors.toList());
